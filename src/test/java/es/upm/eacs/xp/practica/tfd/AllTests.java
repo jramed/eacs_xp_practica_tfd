46590ea5b0e7635c2745d789cde790d375fbfd61
jramed <jramed@gmail.com>
2018-05-14 00:47:33 +0200

    Add a pair of skeletons

---
+++ src/test/java/es/upm/eacs/xp/practica/tfd/AllTests.java
@@ -0,0 +1,16 @@
+package es.upm.eacs.xp.practica.tfd;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	VariableTest.class, 
+	//ConstantTest.class, 
+	//SingleExamTest.class,
+	//CompositeExamTest.class,
+})
+public class AllTests {
+
+}
---
+++ src/test/java/es/upm/eacs/xp/practica/tfd/VariableTest.java
@@ -0,0 +1,11 @@
+package es.upm.eacs.xp.practica.tfd;
+
+import org.junit.Test;
+
+public class VariableTest {
+
+	@Test
+	public void give_when_then() {
+		
+	}
+}
