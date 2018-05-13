09373815950410392d41b2eebfc0c896b327a810
jramed <jramed@gmail.com>
2018-05-14 00:20:32 +0200

    first commit. Eclipse project. Pom.xml and main skeleton

---
+++ src/main/java/es/upm/eacs/xp/practica/tfd/EquationSystemApp.java
@@ -0,0 +1,13 @@
+package es.upm.eacs.xp.practica.tfd;
+
+/**
+ * Hello world!
+ *
+ */
+public class EquationSystemApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
