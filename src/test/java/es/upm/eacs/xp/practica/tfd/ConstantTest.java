1d88b4328151b7b0fc4ea19e3eb21cf239030ac9
jramed <jramed@gmail.com>
2018-05-14 17:15:38 +0200

    Replace assertTrue by assertFalse when in the condition was used the operator !

--- src/test/java/es/upm/eacs/xp/practica/tfd/ConstantTest.java
+++ src/test/java/es/upm/eacs/xp/practica/tfd/ConstantTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -19,7 +20,7 @@ public class ConstantTest {
 		Constant constant1 = new Constant(aFloat(2.5f));
 		Constant constant2 = new Constant(aFloat(4f));
 		assertThat(constant1.getValue(), lessThan(constant2.getValue()));
-		assertTrue(!constant1.equal(constant2));
+		assertFalse(constant1.equal(constant2));
 		
 	}
 	
@@ -28,7 +29,7 @@ public class ConstantTest {
 		Constant constant1 = new Constant(aFloat(700.5f));
 		Constant constant2 = new Constant(aFloat(0.35f));
 		assertThat(constant1.getValue(), greaterThan(constant2.getValue()));
-		assertTrue(!constant1.equal(constant2));
+		assertFalse(constant1.equal(constant2));
 		
 	}
 	
@@ -46,7 +47,7 @@ public class ConstantTest {
 		Constant constant1 = new Constant(aFloat(-9.5f));
 		Constant constant2 = new Constant(aFloat(-0.000001f));
 		assertThat(constant1.getValue(), lessThan(constant2.getValue()));
-		assertTrue(!constant1.equal(constant2));
+		assertFalse(constant1.equal(constant2));
 		
 	}
 	
@@ -55,7 +56,7 @@ public class ConstantTest {
 		Constant constant1 = new Constant(aFloat(-0.0001f));
 		Constant constant2 = new Constant(aFloat(-0.0002f));
 		assertThat(constant1.getValue(), greaterThan(constant2.getValue()));
-		assertTrue(!constant1.equal(constant2));
+		assertFalse(constant1.equal(constant2));
 		
 	}
 	
@@ -72,8 +73,9 @@ public class ConstantTest {
 	public void givenTwoContanstCloseToZero_whenCte1IsGreaterThanCt2_thenNonEquals() {
 		Constant constant1 = new Constant(aFloat(0.0000001f));
 		Constant constant2 = new Constant(aFloat(-0.0000001f));
-		assertTrue(!constant1.equal(constant2));
 		assertThat(constant1.getValue(), greaterThan(constant2.getValue()));
+		assertFalse(constant1.equal(constant2));
+		
 	}
 	
 	@Test
@@ -89,8 +91,8 @@ public class ConstantTest {
 	public void givenTwoContanstEqualToZeroButDifferentSign_whenCte1IsEqualThanCt2_thenEquals() {
 		Constant constant1 = new Constant(aFloat(-0.00f));
 		Constant constant2 = new Constant(aFloat(0.00f));
-		assertThat(constant1.getValue(), equalTo(constant2.getValue()));
-		assertTrue(constant1.equal(constant2));		
+		assertThat(constant1.getValue(), lessThan(constant2.getValue()));
+		assertFalse(constant1.equal(constant2));		
 	}
 	
 	@Test
