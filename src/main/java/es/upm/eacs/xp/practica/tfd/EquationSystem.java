8dd7c8826ecd1d6ed9a537dfb4833b018cb7e0c2
jramed <jramed@gmail.com>
2018-06-20 19:31:43 +0200

    Add equal, getNameSet and Get(index) methods
    Add the test for those methods

--- src/main/java/es/upm/eacs/xp/practica/tfd/EquationSystem.java
+++ src/main/java/es/upm/eacs/xp/practica/tfd/EquationSystem.java
@@ -1,18 +1,23 @@
 package es.upm.eacs.xp.practica.tfd;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class EquationSystem {
 
     List<Equation> equationList;
+    private Set<String> nameSet;
 
     public EquationSystem() {
         this.equationList = new ArrayList<Equation>();
+        this.nameSet = new HashSet<String>();
     }
 
     public void add(Equation equation) {
         this.equationList.add(equation.clon());
+        nameSet.addAll(equation.getNameSet());
     }
 
     public boolean equal(EquationSystem equationSystem2) {
@@ -20,10 +25,34 @@ public class EquationSystem {
             return false;
         }
 
-        for (Equation equation : equationList) {
-            // if (equation)
+        boolean results[] = new boolean[this.equationList.size()];
+        int pos = 0;
+        for (Equation equationOut : equationList) {
+            boolean isEqual = false;
+            for (Equation equationIn : equationSystem2.equationList) {
+                if (equationIn.equal(equationOut)) {
+                    isEqual = true;
+                    break;
+                }
+            }
+            results[pos] = isEqual;
+            pos++;
         }
-        return false;
+
+        boolean resultEqual = true;
+        for (boolean result : results) {
+            System.out.println("previous equality: " + resultEqual + " the result for this term is: " + result);
+            resultEqual &= result;
+        }
+        return resultEqual;
+    }
+
+    public Set<String> getNameSet() {
+        return this.nameSet;
+    }
+
+    public Equation get(int index) {
+        return this.equationList.get(index);
     }
 
 }
--- src/test/java/es/upm/eacs/xp/practica/tfd/EquationSystemTest.java
+++ src/test/java/es/upm/eacs/xp/practica/tfd/EquationSystemTest.java
@@ -1,10 +1,24 @@
 package es.upm.eacs.xp.practica.tfd;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class EquationSystemTest {
+
+    @Test
+    public void givenAnEmptyEquationSytem_whenCheckingEguality_thenEgual() {
+        EquationSystem equationSystem1 = new EquationSystem();
+        EquationSystem equationSystem2 = new EquationSystem();
+
+        assertTrue(equationSystem1.equal(equationSystem2));
+
+    }
 
     @Test
     public void givenAnEmptyEquationSytem_whenAddAnEquation_thenEquationAdded() {
@@ -17,7 +31,54 @@ public class EquationSystemTest {
         equationSystem2.add(equation2);
 
         assertTrue(equationSystem1.equal(equationSystem2));
-
     }
 
+    @Test
+    public void givenAnEquationSystemWithSeveralEquation_whenCheckingEqualityWithAn_thenEqual() {
+        EquationSystem equationSystem1 = new EquationSystem();
+        EquationSystem equationSystem2 = new EquationSystem();
+        Equation equation1 = new EquationBuilder().term(3f).term(-1f, "X").term(-4f, "Y").assign().term(5f, "Z")
+                .term(2f, "X").build();
+        Equation equation2 = equation1.clon();
+        Equation equation3 = new EquationBuilder().term(3f, "X").term(-1f, "Z").term(-4f).assign().term(5f, "Y")
+                .term(2f, "Y").build();
+        Equation equation4 = equation3.clon();
+        equationSystem1.add(equation3);
+        equationSystem1.add(equation1);
+        equationSystem2.add(equation2);
+        equationSystem2.add(equation4);
+
+        assertTrue(equationSystem1.equal(equationSystem2));
+    }
+
+    @Test
+    public void givenAnEquationSystem_whenGetNameSet_thenObtainGetNameSet() {
+        EquationSystem equationSystem1 = new EquationSystem();
+        Equation equation1 = new EquationBuilder().term(3f).term(-1f, "X").term(-4f, "Y").assign().term(5f, "Z")
+                .term(2f, "X").build();
+        Equation equation2 = new EquationBuilder().term(3f, "X").term(-1f, "Z").term(-4f).assign().term(5f, "Y")
+                .term(2f, "Y").build();
+        equationSystem1.add(equation1);
+        equationSystem1.add(equation2);
+        Set<String> myNameSet = new HashSet<String>();
+        myNameSet.add("X");
+        myNameSet.add("Y");
+        myNameSet.add("Z");
+
+        assertThat(myNameSet, equalTo(equationSystem1.getNameSet()));
+    }
+
+    @Test
+    public void givenAnEquationSystem_WhenGetIndex_thenReturnEquationFromThatIndex() {
+        EquationSystem equationSystem1 = new EquationSystem();
+        Equation equation1 = new EquationBuilder().term(3f).term(-1f, "X").term(-4f, "Y").assign().term(5f, "Z")
+                .term(2f, "X").build();
+        Equation equation2 = new EquationBuilder().term(3f, "X").term(-1f, "Z").term(-4f).assign().term(5f, "Y")
+                .term(2f, "Y").build();
+        equationSystem1.add(equation1);
+        equationSystem1.add(equation2);
+
+        assertTrue(equation1.equal(equationSystem1.get(0)));
+        assertTrue(equation2.equal(equationSystem1.get(1)));
+    }
 }
