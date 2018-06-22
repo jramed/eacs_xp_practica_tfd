a62724bd895f7f407981e04d316ca37f854d9883
jramed <jramed@gmail.com>
2018-06-22 19:55:52 +0200

    Refactor: stick to the design and have just members attribute
    PReviously two attribute were used to store the expressions

--- src/main/java/es/upm/eacs/xp/practica/tfd/Equation.java
+++ src/main/java/es/upm/eacs/xp/practica/tfd/Equation.java
@@ -1,128 +1,124 @@
 package es.upm.eacs.xp.practica.tfd;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Equation {
 
-    public Expression leftExpression;
-    public Expression rightExpression;
+    public Map<Side, Expression> members;
 
     public Equation() {
-        this.leftExpression = new Expression();
-        this.rightExpression = new Expression();
+        this.members = new HashMap<Side, Expression>();
+        members.put(Side.LEFT, new Expression());
+        members.put(Side.RIGHT, new Expression());
     }
 
     public Equation(Expression[] expressions) {
         this();
-
-        this.leftExpression.add(expressions[0]);
-        this.rightExpression.add(expressions[1]);
+        members.put(Side.LEFT, expressions[0]);
+        members.put(Side.RIGHT, expressions[1]);
     }
 
     public void add(Term term) {
-        this.leftExpression.add(term);
-        this.rightExpression.add(term);
+        for (Expression expression : this.members.values()) {
+            expression.add(term);
+        }
     }
 
     public void add(Side side, Term term) {
-        if (Side.LEFT == side) {
-            this.leftExpression.add(term);
-        } else {
-            this.rightExpression.add(term);
-        }
+        this.members.get(side).add(term);
     }
 
     public void add(Equation equation) {
-        this.leftExpression.add(equation.leftExpression.clon());
-        this.rightExpression.add(equation.rightExpression.clon());
+        this.members.get(Side.LEFT).add(equation.members.get(Side.LEFT));
+        this.members.get(Side.RIGHT).add(equation.members.get(Side.RIGHT));
     }
 
     public boolean equal(Equation equation) {
-        boolean identical = this.leftExpression.equal(equation.leftExpression)
-                && this.rightExpression.equal(equation.rightExpression);
-        boolean reverse = false;
-        if (!identical) {
-            Expression expressionL = equation.leftExpression.clon();
+        boolean identical1 = this.members.get(Side.LEFT).equal(equation.members.get(Side.LEFT))
+                && this.members.get(Side.RIGHT).equal(equation.members.get(Side.RIGHT));
+        boolean reverse1 = false;
+        if (!identical1) {
+            Expression expressionL = equation.members.get(Side.LEFT).clon();
             expressionL.multiply(-1f);
-            Expression expressionR = equation.rightExpression.clon();
+            Expression expressionR = equation.members.get(Side.RIGHT).clon();
             expressionR.multiply(-1f);
-            reverse = this.leftExpression.equal(expressionR) && this.rightExpression.equal(expressionL);
+            reverse1 = this.members.get(Side.LEFT).equal(expressionR)
+                    && this.members.get(Side.RIGHT).equal(expressionL);
         }
-        return identical || reverse;
+
+        return identical1 || reverse1;
+
     }
 
     public void multiply(float value) {
-        this.leftExpression.multiply(value);
-        this.rightExpression.multiply(value);
+        this.members.get(Side.LEFT).multiply(value);
+        this.members.get(Side.RIGHT).multiply(value);
 
     }
 
     public float getValue(String name) {
-        if (this.leftExpression.hasName(name)) {
-            return this.leftExpression.getValue(name);
-        } else if (this.rightExpression.hasName(name)) {
-            return this.rightExpression.getValue(name);
+        float value = 0.0f;
+        for (Expression expression : this.members.values()) {
+            if (expression.hasName(name)) {
+                return expression.getValue(name);
+            }
         }
-        return 0.0f;
+        return value;
     }
 
     public float getValue(Side side) {
-        if (Side.LEFT == side) {
-            return this.leftExpression.getValue();
-        } else if (Side.RIGHT == side) {
-            return this.rightExpression.getValue();
-        }
-        return 0.0f;
+        float value = 0.0f;
+        value = this.members.get(side).getValue();
+        return value;
     }
 
     public void simplify(Side side) {
-        if (Side.LEFT == side) {
-            this.leftExpression.simplify();
-        } else if (Side.RIGHT == side) {
-            this.rightExpression.simplify();
-        }
+        this.members.get(side).simplify();
     }
 
     public void simplify(Side side, String name) {
-        if (Side.LEFT == side) {
-            this.leftExpression.simplify(name);
-        } else if (Side.RIGHT == side) {
-            this.rightExpression.simplify(name);
-        }
+        this.members.get(side).simplify(name);
     }
 
     public Set<String> getNameSet() {
         Set<String> theNameSet = new HashSet<String>();
-        for (String name : this.leftExpression.getNameSet()) {
-            theNameSet.add(name);
-        }
-        for (String name : this.rightExpression.getNameSet()) {
-            theNameSet.add(name);
+
+        for (Expression expression : this.members.values()) {
+            theNameSet.addAll(expression.getNameSet());
         }
 
         return theNameSet;
+
     }
 
     public Equation clon() {
-        Expression[] expressions = new Expression[] { this.leftExpression.clon(), this.rightExpression.clon() };
-        return new Equation(expressions);
+        Expression[] expressions1 = new Expression[] { this.members.get(Side.LEFT).clon(),
+                this.members.get(Side.RIGHT).clon() };
+        return new Equation(expressions1);
     }
 
     @Override
     public String toString() {
-        return this.leftExpression.toString() + " = " + this.rightExpression.toString();
+        String result1 = this.members.get(Side.LEFT).toString() + " = " + this.members.get(Side.RIGHT).toString();
+        return result1;
+
     }
 
     public void apply(String name, float value) {
-        this.leftExpression.apply(value, name);
-        this.rightExpression.apply(value, name);
+        for (Side side : Side.values()) {
+            if (this.members.get(side).hasName(name)) {
+                this.members.get(side).apply(name, value);
+            }
+        }
     }
 
     public void invert() {
-        Expression copy = this.leftExpression.clon();
-        this.leftExpression = this.rightExpression.clon();
-        this.rightExpression = copy;
+        Expression copyLeft = this.members.get(Side.LEFT).clon();
+        this.members.put(Side.LEFT, this.members.get(Side.RIGHT).clon());
+        this.members.put(Side.RIGHT, copyLeft);
     }
 
 }
